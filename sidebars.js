d8dea4404a647d685250b37f9ba69f43e6bbbf5b
clywyk <clywyk@gmail.com>
2024-10-28 12:22:33 +0100

    DOC-437 add acceptable id documents to doc center

--- sidebars.js
+++ sidebars.js
@@ -554,6 +554,7 @@ module.exports = {
           ],
         },
         "partnership/document-center/proof-address/index",
+        "partnership/document-center/identity/index",
       ],
     },
     {
